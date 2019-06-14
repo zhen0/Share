6f81181bd89e8303d491a68ab84094569420df51
Jenny <jenniferhelengrange@gmail.com>
2019-06-14 08:32:57 -0400

    starts adding kit

--- Client/reducers/kit.js
+++ Client/reducers/kit.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 
 //types
-
+const ADDKIT = "ADD_KIT";
 //creators
+const updateKitList = kit => ({ type: ADDKIT, kit });
 
 // thunks
+export const sendKit = newKit => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.post("/api/kit", newKit);
+
+      dispatch(updateKitList(data));
+    } catch (err) {
+      console.log("problem adding new Kit at kitReducer", err);
+    }
+  };
+};
 
 //state
 
-function kitReducer(state = {}, action) {
+function kitReducer(state = { kit: {} }, action) {
+  switch (action.type) {
+    case ADDKIT:
+      return { ...state, kit: action.kit };
+  }
   return state;
 }
 
--- expoApp/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import { Text, View, Image } from "react-native";
-import Root from "./components/root";
-
-export default class App extends React.Component {
-  render() {
-    return <Root />;
-  }
-}
